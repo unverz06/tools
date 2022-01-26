355e382e5df916c2cc7884586c57d162728e11e6
Thomas <thomas@apsulis.io>
2022-01-26 22:01:45 +0100

    add(): import data sample

---
+++ import_data_sample.py
@@ -0,0 +1,14 @@
+from engine.head import *
+
+filename = 'data/' + 'BTCUSDT_DATA_2022.json'
+
+if os.path.exists(filename):
+    df = pd.read_json(path_or_buf=filename, orient='index')
+    # df = df['2020-01-01':]
+    # df = df['2021-01-01':'2021-05-01']
+    print("\nData pandas loaded 100%...\n")
+else:
+    print("Pas de JSON à charger..")
+    exit()
+
+print(df)
