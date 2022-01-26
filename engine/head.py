33484ffe47646f8fa12f853cd2145450ad0241c5
Thomas <thomas@apsulis.io>
2022-01-26 22:00:37 +0100

    chore()

--- engine/head.py
+++ engine/head.py
@@ -1,15 +1,10 @@
 import pandas as pd
 from binance.client import Client
-import ta
 import json
 from datetime import datetime
-from time import *
-from sys import stdout
-import matplotlib.pyplot as plt
-import numpy as np
+# import numpy as np
 import os
-from math import *
-import ccxt
+# import ccxt
 from sklearn.cluster import KMeans
 import warnings
 import math
